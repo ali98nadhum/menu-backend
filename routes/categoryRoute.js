4f9e80d45fd9223dd0ef2e2eb0e5827d37112814
Ali nadhum <alinadhum1998@gmail.com>
2025-01-21 00:40:59 +0300

    add getOneCategory route

--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -1,9 +1,13 @@
-const { getAllCategory, createCategory } = require("../controllers/categoryController");
+const { getAllCategory, createCategory, getOneCategory } = require("../controllers/categoryController");
 
 const router = require("express").Router();
 
 
-router.route("/").get(getAllCategory)
-router.route("/").post(createCategory);
+router.route("/")
+.get(getAllCategory)
+.post(createCategory);
+
+router.route("/:id")
+.get(getOneCategory)
 
 module.exports = router;
