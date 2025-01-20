171cbd8a2cbaa4120cbf4ced16a8c33ae70d310b
Ali nadhum <alinadhum1998@gmail.com>
2025-01-20 20:47:40 +0300

    create category model

---
+++ models/category.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const join = require("join");
+
+const CategorySchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "category title is required"],
+      unique: true,
+      trim: true,
+      minlength: [3, "category must be at least 3 characters"],
+      maxlength: [50, "category must be at most 50 characterss"],
+    },
+
+    image: {
+      type: Object,
+      required: [true, "category image is required"],
+      default: {
+        url: "",
+        publicId: null,
+      },
+    },
+  },
+  { timestamps: true }
+);
+
+const CategoryModel = mongoose.model("CategoryModel", CategorySchema);
+
+module.exports = {
+  CategoryModel,
+};
