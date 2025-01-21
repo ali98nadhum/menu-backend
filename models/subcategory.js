e9c8e8eca956a0c19d24fa0da5267fb8ef6ddac0
Ali nadhum <alinadhum1998@gmail.com>
2025-01-21 20:35:02 +0300

    add subCategoryModel

--- models/subcategory.js
+++ models/subcategory.js
@@ -1,9 +1,26 @@
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
-// const subCategorySchema = new mongoose.Schema({
-//     title: {
-//         type: String,
-//         required: [true , "subCategory title is required"],
-//         unique: true,
-//     }
-// })
+const subCategorySchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, "subCategory title is required"],
+    unique: true,
+    trim: true,
+    minlength: [3, "category must be at least 3 characters"],
+  },
+  
+  price: {
+    type: Number,
+    required: [true, "subCategory price is required"],
+    min: [0, "Price must be greater than or equal to 0"],
+    set: (value) => Math.round(value),
+  }
+} , {timestamps : true});
+
+
+const SubCategoryModel = mongoose.model("SubCategoryModel" , subCategorySchema);
+
+
+module.exports = {
+    SubCategoryModel
+}
