17a26ba30cf9128f758efe453fa1eda037651de2
Ali nadhum <alinadhum1998@gmail.com>
2025-01-22 20:16:30 +0300

    edit route link in index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(cors({
 
   // Routes
   app.use("/api/v1/category" , require("./routes/categoryRoute"));
-  app.use("/api/v1/subcategory" , require("./routes/productRoute"));
+  app.use("/api/v1/product" , require("./routes/productRoute"));
 
 
   // Run Server
