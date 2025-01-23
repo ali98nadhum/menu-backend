76adcc8b375f7da6019e02f2fff7c272c6693ec2
Ali nadhum <alinadhum1998@gmail.com>
2025-01-23 04:10:53 +0300

    add validateUpdateProduct fun

--- middlewares/productValidation.js
+++ middlewares/productValidation.js
@@ -37,7 +37,39 @@ const validateCreateProduct = (data) => {
 
 
 
+const validateUpdateProduct = (data) => {
+  const schema = Joi.object({
+    title: Joi.string()
+      .min(3)
+      .max(50)
+      .messages({
+        "string.base": "Title must be a string",
+        "string.empty": "Title is required",
+        "string.min": "Title must be at least 3 characters long",
+        "string.max": "Title must be at most 50 characters long",
+      }),
+    price: Joi.number()
+      .min(0)
+      .messages({
+        "number.base": "Price must be a number",
+        "number.min": "Price must be greater than or equal to 0"
+      }),
+
+      category: Joi.string() // التحقق من أن category هو معرف (ID)
+      .messages({
+        "string.base": "Category must be a string",
+        "string.empty": "Category is required",
+      }),
+
+  });
+
+  return schema.validate(data, { abortEarly: false });
+};
+
+
+
 
 module.exports = {
   validateCreateProduct,
+  validateUpdateProduct,
 }
