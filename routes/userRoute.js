f6962def5bb0d997444e0c0a7108cf16a4589560
Ali nadhum <alinadhum1998@gmail.com>
2025-01-24 03:31:26 +0300

    edit users route

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -3,7 +3,7 @@ const { getAllUsers } = require("../controllers/userController");
 const router = require("express").Router();
 
 
-router.route("/").post(getAllUsers)
+router.route("/").get(getAllUsers)
 
 
 module.exports = router;
