39c0800154055817d282c15b9801d5186086fcf3
Ali nadhum <alinadhum1998@gmail.com>
2025-01-25 04:27:43 +0300

    add verifyToken to auth route

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,9 +1,9 @@
 const { registerAdmin, loginAdmin } = require("../controllers/authController");
-
 const router = require("express").Router();
+const { verifyToken } = require("../middlewares/verifyToken")
 
 
-router.route("/register").post(registerAdmin)
+router.route("/register").post(verifyToken ,registerAdmin)
 router.route("/login").post(loginAdmin)
 
 
