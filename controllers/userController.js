7b7771dc1b64c1e43d6b680ebbad3697781acf9b
Ali nadhum <alinadhum1998@gmail.com>
2025-01-25 03:35:02 +0300

    add getUserById fun

--- controllers/userController.js
+++ controllers/userController.js
@@ -15,3 +15,20 @@ module.exports.getAllUsers = asyncHandler(async(req , res) => {
     const users = await UserModel.find();
     res.status(200).json({data: users})
 })
+
+
+
+// ==================================
+// @desc Get user by id
+// @route /api/v1/users?:id
+// @method GET
+// @access private ( only admin )
+// ==================================
+module.exports.getUserById = asyncHandler(async (req , res) => {
+    const user = await UserModel.findById(req.params.id);
+    if(!user){
+        return res.status(404).json({message: "No user for this id"})
+    }
+
+    res.status(200).json({data: user});
+})
