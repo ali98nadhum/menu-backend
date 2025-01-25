b6fa62771b541c8336ee4aabb0f8a272f280730d
Ali nadhum <alinadhum1998@gmail.com>
2025-01-25 04:23:14 +0300

    add verifyToken to product route

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,16 +1,16 @@
 const { getAllProducts, createProduct, getOneProduct, deleteProduct, updateProduct } = require("../controllers/productController");
-
 const router = require("express").Router();
+const { verifyToken } = require("../middlewares/verifyToken");
 
 
 router.route("/")
 .get(getAllProducts)
-.post(createProduct)
+.post(verifyToken , createProduct)
 
 router.route("/:id")
 .get(getOneProduct)
-.delete(deleteProduct)
-.put(updateProduct)
+.delete(verifyToken , deleteProduct)
+.put( verifyToken,updateProduct)
 
 
 module.exports = router;
