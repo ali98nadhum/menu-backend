850b30854930096d40cfae80db6251971250b6bd
Ali nadhum <alinadhum1998@gmail.com>
2025-01-25 04:24:45 +0300

    add verifyToken to user route

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,12 +1,12 @@
 const { getAllUsers, getUserById, deleteUser } = require("../controllers/userController");
-
 const router = require("express").Router();
+const { verifyToken } = require("../middlewares/verifyToken");
 
 
-router.route("/").get(getAllUsers)
+router.route("/").get( verifyToken, getAllUsers)
 router.route("/:id")
-.get(getUserById)
-.delete(deleteUser)
+.get(verifyToken , getUserById)
+.delete(verifyToken , deleteUser)
 
 
 module.exports = router;
